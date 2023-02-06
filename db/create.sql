3ac2e1b915a0967f086baae5628841e9e8ea4641
thomas <74536705+thomashoog@users.noreply.github.com>
2023-02-06 10:08:58 +0000

    alle producten erin

--- db/create.sql
+++ db/create.sql
@@ -11,9 +11,9 @@ CREATE TABLE products (
 );
 
 --CREATE TABLE reviews (
-  --id INTEGER PRIMARY KEY AUTOINCREMENT,
-  --person_id INTEGER,
- -- review TEXT,
+--id INTEGER PRIMARY KEY AUTOINCREMENT,
+--person_id INTEGER,
+--review TEXT,
   
 --)
 
@@ -27,9 +27,13 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Album 1', 'Pop album uitgebracht in 2019', '816905633-0', 21);
-insert into products (name, description, code, price) values ('Album 2', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 19);
-insert into products (name, description, code, price) values ('Album 3', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 20);
-insert into products (name, description, code, price) values ('Album 4', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 17);
-insert into products (name, description, code, price) values ('Album 5', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 25);
-insert into products (name, description, code, price) values ('Album 6', 'Gezellig feest muziek', '492662523-7', 26);
+insert into products (name, description, code, price) values ('Grijze spijkerbroek', 'Pop album uitgebracht in 2019', '816905633-0', 29.99);
+insert into products (name, description, code, price) values ('Blauwe spijkerbroek', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 29.99);
+insert into products (name, description, code, price) values ('Zwarte cargo broek', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 24.99);
+insert into products (name, description, code, price) values ('Groen shirt', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 14.99);
+insert into products (name, description, code, price) values ('Witte broek', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 24.99);
+insert into products (name, description, code, price) values ('Rode trui', 'Gezellig feest muziek', '492662523-7', 14.99);
+insert into products (name, description, code, price) values ('Zwarte handschoenen', 'Gezellig feest muziek', '492662523-7', 4.99);
+insert into products (name, description, code, price) values ('Zwarte muts', 'Gezellig feest muziek', '492662523-7', 9.99);
+insert into products (name, description, code, price) values ('Groene schoenen', 'Gezellig feest muziek', '492662523-7', 71.99);
+insert into products (name, description, code, price) values ('Zwarte slippers', 'Gezellig feest muziek', '492662523-7', 14.99);
