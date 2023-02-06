9a25bac797744776991508b92d15423f8f28a7f1
thomas <74536705+thomashoog@users.noreply.github.com>
2023-02-06 11:06:45 +0000

    update tabellen

--- db/create.sql
+++ db/create.sql
@@ -10,11 +10,31 @@ CREATE TABLE products (
   price NUMERIC(10, 2)
 );
 
---CREATE TABLE reviews (
---id INTEGER PRIMARY KEY AUTOINCREMENT,
---person_id INTEGER,
---review TEXT,
-  
+CREATE TABLE reviews (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  review TEXT
+);
+
+CREATE TABLE betalingen (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  betaalmethode TEXT
+);
+
+CREATE TABLE bestellingen (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  adres TEXT,
+  betaalmethode TEXT
+);
+
+CREATE TABLE adres (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  persoon TEXT,
+  adres TEXT
+);
+
 --)
 
 
@@ -27,6 +47,8 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
+
+-- Producten
 insert into products (name, description, code, price) values ('Grijze spijkerbroek', 'Pop album uitgebracht in 2019', '816905633-0', 29.99);
 insert into products (name, description, code, price) values ('Blauwe spijkerbroek', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 29.99);
 insert into products (name, description, code, price) values ('Zwarte cargo broek', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 24.99);
@@ -37,3 +59,37 @@ insert into products (name, description, code, price) values ('Zwarte handschoen
 insert into products (name, description, code, price) values ('Zwarte muts', 'Gezellig feest muziek', '492662523-7', 9.99);
 insert into products (name, description, code, price) values ('Groene schoenen', 'Gezellig feest muziek', '492662523-7', 71.99);
 insert into products (name, description, code, price) values ('Zwarte slippers', 'Gezellig feest muziek', '492662523-7', 14.99);
+
+-- Reviews
+insert into reviews (name, review) values ('Jacco Ren','Valt niet te groot, past precies.');
+insert into reviews (name, review) values ('Roelof Looper','Mooie kleuren.');
+insert into reviews (name, review) values ('Anthonie Van Wart','Valt groot haal 1 maat kleiner dan dat je hebt.');
+insert into reviews (name, review) values ('Stijn Dul','Fijn materiaal makkelijk om in te bewegen.');
+insert into reviews (name, review) values ('Arie Benedix','Heel erg zachte stof.');
+insert into reviews (name, review) values ('Claes Mont','Precies wat ik zocht.');
+insert into reviews (name, review) values ('Anthonie Goes','Hele snelle bezorging.');
+insert into reviews (name, review) values ('Roelof Looper','Erg goedkoop.');
+insert into reviews (name, review) values ('Anthonie Van Wart','Ik heb na mijn eerste bestelling de site gelijk aangeraden aan mijn familie.');
+insert into reviews (name, review) values ('Lukas Troost','Kwam veel later binnen dan bij de tracking stond.');
+insert into reviews (name, review) values ('Sven Van Hee','Ben treveden met mijn bestelling.');
+insert into reviews (name, review) values ('Adriaan Napper','Precies wat ik zocht.');
+
+-- Betalingen
+insert into betalingen (name, betaalmethode) values ('Jacco Ren','ABN AMRO');
+insert into betalingen (name, betaalmethode) values ('Roelof Looper','ING');
+insert into betalingen (name, betaalmethode) values ('Anthonie Van Wart','ING');
+insert into betalingen (name, betaalmethode) values ('Stijn Dul','ABN AMRO');
+insert into betalingen (name, betaalmethode) values ('Arie Benedix','ABN AMRO');
+insert into betalingen (name, betaalmethode) values ('Claes Mont','ING');
+insert into betalingen (name, betaalmethode) values ('Anthonie Goes','Rabobank');
+insert into betalingen (name, betaalmethode) values ('Roelof Looper','Rabobank');
+insert into betalingen (name, betaalmethode) values ('Anthonie Van Wart','ING');
+insert into betalingen (name, betaalmethode) values ('Lukas Troost','ING');
+insert into betalingen (name, betaalmethode) values ('Sven Van Hee','ING');
+insert into betalingen (name, betaalmethode) values ('Adriaan Napper','ABN AMRO');
+
+-- Bestellingen
+insert into reviews (name) values ('past precies');
+
+-- Adres
+insert into reviews (name) values ('past precies');
