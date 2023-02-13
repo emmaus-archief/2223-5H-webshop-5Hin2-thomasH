8886bd8f6bed3e5a7fed3bf7060c592840cfe2ec
thomas <74536705+thomashoog@users.noreply.github.com>
2023-02-13 11:24:53 +0000

    rest van foto's en beschrijvingen producten

--- db/create.sql
+++ db/create.sql
@@ -2,6 +2,8 @@
 -- create tables
 --
 
+-- koppel de id's tussen de tabellen
+
 CREATE TABLE products (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   code VARCHAR(15),
@@ -13,9 +15,10 @@ CREATE TABLE products (
 CREATE TABLE reviews (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
-  review TEXT
+  review TEXT,
+  product_id INTEGER -- verwijs naar product
 );
-
+  
 CREATE TABLE betalingen (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
@@ -26,16 +29,20 @@ CREATE TABLE bestellingen (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
   item TEXT,
-  adres TEXT,
+  adres_id INTEGER, -- verwijs naar adres.adres
   betaalmethode TEXT
 );
 
 CREATE TABLE adres (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT,
-  adres  TEXT
+  adres TEXT
 );
 
+CREATE TABLE personen (
+  betaalmethodes,
+  merk
+);
 --)
 
 
@@ -50,23 +57,23 @@ CREATE TABLE adres (
 
 
 -- Producten
-insert into products (name, description, code, price) values ('Grijze spijkerbroek', 'Slim fit spijkerbroek', '816905633-0', 29.99);
-insert into products (name, description, code, price) values ('Blauwe spijkerbroek', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 29.99);
-insert into products (name, description, code, price) values ('Zwarte cargo broek', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 24.99);
-insert into products (name, description, code, price) values ('Groen shirt', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 14.99);
-insert into products (name, description, code, price) values ('Gele trui', 'Gezellig feest muziek', '492662523-7', 29.99);
-insert into products (name, description, code, price) values ('Witte broek', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 24.99);
-insert into products (name, description, code, price) values ('Rode trui', 'Gezellig feest muziek', '492662523-7', 14.99);
-insert into products (name, description, code, price) values ('Witte sokken', 'Gezellig feest muziek', '492662523-7', 4.99);
-insert into products (name, description, code, price) values ('Zwarte handschoenen', 'Gezellig feest muziek', '492662523-7', 4.99);
-insert into products (name, description, code, price) values ('Zwarte muts', 'Gezellig feest muziek', '492662523-7', 9.99);
-insert into products (name, description, code, price) values ('Groene schoenen', 'Gezellig feest muziek', '492662523-7', 71.99);
-insert into products (name, description, code, price) values ('Zwarte slippers', 'Gezellig feest muziek', '492662523-7', 14.99);
+insert into products (name, description, code, price) values ('Grijze spijkerbroek', 'Slim fit spijkerbroek.', '816905633-0', 29.99);
+insert into products (name, description, code, price) values ('Blauwe spijkerbroek', 'Loose fit spijkerbroek.', '077030122-3', 29.99);
+insert into products (name, description, code, price) values ('Zwarte cargo broek', 'Loose fit cargo met veel zakken.', '445924201-X', 24.99);
+insert into products (name, description, code, price) values ('Groen shirt', 'Slim fit shirt met uitrekbare stof.', '693155505-7', 14.99);
+insert into products (name, description, code, price) values ('Gele trui', 'Relaxed fit trui van zachte stof.', '492662523-7', 29.99);
+insert into products (name, description, code, price) values ('Witte broek', 'Tapered fit broek met pijpen op enkelhoogte.', '686928463-6', 24.99);
+insert into products (name, description, code, price) values ('Rode trui', 'Relaxed fit trui van zachte stof.', '492662523-7', 14.99);
+insert into products (name, description, code, price) values ('Witte sokken', 'Lange sokken van katoen.', '492662523-7', 4.99);
+insert into products (name, description, code, price) values ('Zwarte handschoenen', 'Elastische handschoenen van katoen.', '492662523-7', 4.99);
+insert into products (name, description, code, price) values ('Zwarte muts', 'Muts met gemêleerd patroon.', '492662523-7', 9.99);
+insert into products (name, description, code, price) values ('Groene schoenen', 'Schoenen met dikke zolen.', '492662523-7', 71.99);
+insert into products (name, description, code, price) values ('Zwarte slippers', 'Badslippers voor in de zomer of zwembad.', '492662523-7', 14.99);
 
 -- Reviews
 insert into reviews (name, review) values ('Jacco Ren','Valt niet te groot, past precies.');
 insert into reviews (name, review) values ('Roelof Looper','Mooie kleuren.');
-insert into reviews (name, review) values ('Anthonie Van Wart','Valt groot haal 1 maat kleiner dan dat je hebt.');
+insert into reviews (name, review) values ('Anthonie Van Wart','Niet dezelfde kleur als op de foto.');
 insert into reviews (name, review) values ('Stijn Dul','Fijn materiaal makkelijk om in te bewegen.');
 insert into reviews (name, review) values ('Arie Benedix','Heel erg zachte stof.');
 insert into reviews (name, review) values ('Claes Mont','Precies wat ik zocht.');
